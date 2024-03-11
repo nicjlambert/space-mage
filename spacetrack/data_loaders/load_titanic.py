8e4e9b307173a4972a40ce1470c688aa1e1556e3
lambert <lambni85@outlook.com>
2024-03-11 09:59:12 +0000

    added github secrets to wrkflow

--- spacetrack/data_loaders/load_titanic.py
+++ spacetrack/data_loaders/load_titanic.py
@@ -1,8 +1,9 @@
-import io
 import os
 import pandas as pd
 import requests
 from pandas import DataFrame
+from io import StringIO
+from urllib.parse import urlencode
 
 if 'data_loader' not in globals():
     from mage_ai.data_preparation.decorators import data_loader
@@ -12,20 +13,32 @@ if 'test' not in globals():
 
 @data_loader
 def load_data_from_api(**kwargs) -> DataFrame:
-    """
-    Template for loading data from API
-    """
-    password = os.environ.get('SPACETRACK_API_PW')
-    identity = os.environ.get('SPACETRACK_USER')
+    password = os.environ.get('SPACETRACK_API_PW', '')
+    identity = os.environ.get('SPACETRACK_USER', '')
+    
+    # Check if either password or identity is an empty string, indicating they weren't set correctly
+    if not password or not identity:
+        raise Exception("Environment variables for SpaceTrack API credentials are not set or empty.")
 
     url = "https://www.space-track.org/ajaxauth/login"
 
-    payload = 'identity={}&password={password}&query=https%3A%2F%2Fwww.space-track.org%2Fbasicspacedata%2Fquery%2Fclass%2Fsatcat%2Fnorad_cat_id%2F4321%2Fformat%2Fcsv'
+    # Dynamically include the identity and password in the payload
+    query = "https://www.space-track.org/basicspacedata/query/class/satcat/norad_cat_id/4321/format/csv"
+    payload = urlencode({
+        'identity': identity,
+        'password': password,
+        'query': query
+    })
+
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
-    return pd.read_csv(response.text)
+    # Check response status and handle potential errors before attempting to read the CSV data
+    if response.status_code != 200:
+        raise Exception(f"Failed to load data from API. Status code: {response.status_code}, Response: {response.text}")
+
+    return pd.read_csv(StringIO(response.text))
 
 
 @test
